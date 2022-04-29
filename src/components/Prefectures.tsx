67ee695153edfe861d566d641de1958095d65cfc
mr_ozin <mrozin.dev@gmail.com>
2022-04-29 13:45:27 +0900

    split components

--- src/components/Prefectures.tsx
+++ src/components/Prefectures.tsx
@@ -1,30 +1,51 @@
+import { ChangeEvent, memo } from 'react'
 import { useCheckedList, usePrefectures } from '../lib/hooks'
 
 export function PrefecturesList() {
   const { pref, loading } = usePrefectures()
   const { handleCheck } = useCheckedList()
   return (
-    <>
+    <fieldset>
+      <legend>都道府県</legend>
       {loading ? (
         <div>loading...</div>
       ) : (
-        <fieldset>
-          <legend>都道府県</legend>
-          <div className="pref-list-container">
-            {pref?.result?.map((pref) => (
-              <div key={pref.prefCode} className="pref-list-item">
-                <input
-                  type={'checkbox'}
-                  id={pref.prefName}
-                  value={pref.prefCode}
-                  onChange={handleCheck}
-                />
-                <label htmlFor={pref.prefName}>{pref.prefName}</label>
-              </div>
-            ))}
-          </div>
-        </fieldset>
+        <div className="pref-list-container">
+          {pref?.result?.map((pref) => (
+            <PrefectureItem
+              key={pref.prefCode}
+              pref={pref}
+              handler={handleCheck}
+            />
+          ))}
+        </div>
       )}
-    </>
+    </fieldset>
   )
 }
+
+type PrefectureItemProps = {
+  pref: {
+    prefName: string
+    prefCode: number
+  }
+  handler: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+export const PrefectureItem = memo(function PrefectureComponent({
+  pref,
+  handler,
+}: PrefectureItemProps) {
+  const { prefName, prefCode } = pref
+  return (
+    <div className="pref-list-item">
+      <input
+        type={'checkbox'}
+        id={prefName}
+        value={prefCode}
+        onChange={handler}
+      />
+      <label htmlFor={prefName}>{prefName}</label>
+    </div>
+  )
+})
